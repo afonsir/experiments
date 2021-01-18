75f5bdcaeeb71b9fd3309e3fb0eac869dff63c5e
afonsir17 <afonsir17@gmail.com>
2021-01-18 12:18:33 -0300

    Adding html_statement for customer

--- ruby/refactoring/video-store/Customer.rb
+++ ruby/refactoring/video-store/Customer.rb
@@ -10,6 +10,21 @@ class Customer
     @rentals << arg
   end
 
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1>"
+    result << "<p>"
+    @rentals.each do |element|
+      # show figures for this rental
+      result << "#{element.movie.title}: #{element.charge}<br>"
+    end
+    result << "</p>"
+    # add footer lines
+    result << "<p>You owe <em>#{total_charge}</em></p>"
+    result << "<p>On this rental you earned <em>#{total_frequent_renter_points}</em> "
+    result << "frequent rental points</p>"
+    result
+  end
+
   def statement
     result = "Rental Record for #{name}\n"
 
