dbf4ede2022ee0f001f4cf4641b34f53ff2cfee5
afonsir17 <afonsir17@gmail.com>
2021-01-22 20:56:00 -0300

    Moving hash_initializers logic to Class class, through a module

--- ruby/refactoring/introduce-class-annotation/search_criteria.rb
+++ ruby/refactoring/introduce-class-annotation/search_criteria.rb
@@ -1,14 +1,3 @@
 class SearchCriteria
-  class << self
-    def hash_initializer(*attribute_names)
-      define_method(:initialize) do |*args|
-        data = args.first || {}
-        attribute_names.each do |attribute_name|
-          instance_variable_set "@#{attribute_name}", data[attribute_name]
-        end
-      end
-    end
-  end
-
   hash_initializer :author_id, :publisher_id, :isbn
 end
