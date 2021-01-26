f4e49d0e049dbeb563ade213d597588c1587da48
afonsir17 <afonsir17@gmail.com>
2021-01-26 17:01:18 -0300

    Applying Naming Optional Parameters to Book

--- ruby/refactoring/naming-optional-parameters/book.rb
+++ ruby/refactoring/naming-optional-parameters/book.rb
@@ -1,13 +1,16 @@
 class Book
   class << self
-    def find(selector, conditions = '', *joins)
+    def find(selector, hash = {})
+      hash[:joins] ||= []
+      hash[:conditions] ||= ''
+
       sql = ['SELECT * FROM books']
-      joins.each do |join_table|
+      hash[:joins].each do |join_table|
         sql << "LEFT OUTER JOIN #{join_table} ON"
         sql << "books.#{join_table.to_s.chop}_id"
         sql << " = #{join_table}.id"
       end
-      sql << "WHERE #{conditions}" unless conditions.empty?
+      sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
       sql << 'LIMIT 1' if selector == :first
       # connection.find(sql.join(' '))
       sql.join(' ')
