7bea90b02f2dac5a457312391a448997b603a6b3
AfonSir <afonsir17@gmail.com>
2021-02-02 19:53:44 -0300

    Applying Isolate Dynamic Receptor

---
+++ ruby/refactoring/isolate_dynamic_receptor/message_collector.rb
@@ -0,0 +1,15 @@
+class MessageCollector
+  instance_methods.each do |meth|
+    undef_method meth unless meth =~ /^(__|inspect)/
+  end
+
+  def messages
+    @messages ||= []
+  end
+
+  def method_missing(sym, *args)
+    messages << [sym, args] unless sym == :is_a?
+    self
+  end
+
+end
--- ruby/refactoring/isolate_dynamic_receptor/recorder.rb
+++ ruby/refactoring/isolate_dynamic_receptor/recorder.rb
@@ -1,25 +1,16 @@
 class Recorder
-  instance_methods.each do |meth|
-    undef_method meth unless meth =~ /^(__|inspect)/
-  end
-
-  def messages
-    @messages ||= []
-  end
-
-  def method_missing(sym, *args)
-    messages << [sym, args] unless sym == :is_a?
-    self
-  end
-
   def play_for(obj)
-    messages.inject(obj) do |result, message|
+    @record.messages.inject(obj) do |result, message|
       result.send message.first, *message.last
     end
   end
 
+  def record
+    @record ||= MessageCollector.new
+  end
+
   def to_s
-    messages.inject([]) do |result, message|
+    @record.messages.inject([]) do |result, message|
       result << "#{message.first}(args: #{message.last.inspect})"
     end.join('.')
   end
