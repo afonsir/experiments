b543f589bd31c4eefda49985b1b73039fba0b891
AfonSir <afonsir17@gmail.com>
2021-02-02 19:37:54 -0300

    Adding Recorder and CommandCenter classes (before apply Isolate Dynamic Receptor)

---
+++ ruby/refactoring/isolate_dynamic_receptor/command_center.rb
@@ -0,0 +1,11 @@
+class CommandCenter
+  def start(command_string)
+    puts "start: #{command_string}"
+    self
+  end
+
+  def stop(command_string)
+    puts "stop: #{command_string}"
+    self
+  end
+end
