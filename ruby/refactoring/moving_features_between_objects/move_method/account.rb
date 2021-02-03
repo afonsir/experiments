684ea5a59d1b98b33be9caeac36bde4330586cce
AfonSir <afonsir17@gmail.com>
2021-02-03 13:17:45 -0300

    Applying Move Method in overdraft_charge (move logic to AccountType class)

--- ruby/refactoring/moving_features_between_objects/move_method/account.rb
+++ ruby/refactoring/moving_features_between_objects/move_method/account.rb
@@ -1,12 +1,6 @@
 class Account
   def overdraft_charge
-    if @account_type.premium?
-      result = 10
-      result += (@days_overdrawn - 7) * 0.85 if @days_overdrawn > 7
-      result
-    else
-      @days_overdrawn * 1.75
-    end
+    @account_type.overdraft_charge(@days_overdrawn)
   end
 
   def bank_charge
