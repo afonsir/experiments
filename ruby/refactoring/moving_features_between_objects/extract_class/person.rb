ecd373c97ca9a095856f1c79ef163736f94bf509
AfonSir <afonsir17@gmail.com>
2021-02-03 17:01:39 -0300

    Applying Extract Class in Person

--- ruby/refactoring/moving_features_between_objects/extract_class/person.rb
+++ ruby/refactoring/moving_features_between_objects/extract_class/person.rb
@@ -1,9 +1,11 @@
 class Person
-  attr_reader :name
-  attr_accessor :office_area_code
-  attr_accessor :office_number
+  attr_reader :name, :office_telephone
+
+  def initialize
+    @office_telephone = TelephoneNumber.new
+  end
 
   def telephone_number
-    "(#{@office_area_code}) #{@office_number}"
+    @office_telephone.telephone_number
   end
 end
